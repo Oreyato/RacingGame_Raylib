defdb051c8fe3500fbbeccabc35ea35e11ad9866
oreyato <oreyato.pro@gmail.com>
2023-01-03 11:30:02 +0100

    ref: from BrickBreaker to RacingGame

---
+++ RacingGame/Consts.cpp
@@ -0,0 +1,24 @@
+#include "Consts.h"
+
+// Screen
+const int Consts::WIDTH_SCREEN{ 800 };
+const int Consts::HEIGHT_SCREEN{ 600 };
+
+// Rules
+const int Consts::MAX_LIFE{ 3 };
+
+// Car
+const int Consts::SIZE_CAR{ 32 };
+const int Consts::SPEED_CAR{ 6 };
+
+// Tracks
+const int Consts::WIDTH_TRACK{ 40 };
+const int Consts::HEIGHT_TRACK{ 40 };
+
+const int Consts::COLUMN_TRACKS{ 20 };
+const int Consts::ROW_TRACKS{ 15 };
+const int Consts::SPACING_TRACKS{ 1 };
+
+
+
+
---
+++ RacingGame/Consts.h
@@ -0,0 +1,27 @@
+#ifndef CONSTS_H
+#define CONSTS_H
+
+class Consts
+{
+public:
+	// Screen
+	static const int WIDTH_SCREEN;
+	static const int HEIGHT_SCREEN;
+
+	// Rules
+	static const int MAX_LIFE;
+
+	// Car
+	static const int SIZE_CAR;
+	static const int SPEED_CAR;
+
+	// Tracks
+	static const int WIDTH_TRACK;
+	static const int HEIGHT_TRACK;
+
+	static const int COLUMN_TRACKS;
+	static const int ROW_TRACKS;
+	static const int SPACING_TRACKS;
+};
+
+#endif // CONSTS_H
